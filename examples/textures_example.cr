d85f6e001aec936ecf63c6b0a42fc7157930c8d6
Matt Swieboda <mswiebod@gmail.com>
2020-03-21 22:50:09 -0500

    adds sprite start_frame and end_frame

--- examples/textures_example.cr
+++ examples/textures_example.cr
@@ -13,11 +13,20 @@ class TexturesExample < Game
         width:    128,
         height:   128,
         loops:    false,
-        fps:      30,
+      },
+      {
+        name:        :dust,
+        filename:    "./assets/explosion.png",
+        width:       128,
+        height:      128,
+        loops:       true,
+        fps:         3,
+        start_frame: 40,
+        end_frame:   48,
       },
     ])
 
-    @sprite = Sprite.get(:explosion)
+    @explosion = Sprite.get(:explosion)
 
     @texture = Texture.load("./assets/crystal_icon.png")
 
@@ -30,17 +39,20 @@ class TexturesExample < Game
       color: Color::Black
     )
 
+    @dust = Sprite.get(:dust)
+
     @x = 0
     @y = 0
   end
 
   def update
-    @sprite.update(frame_time) unless @x == 0 && @y == 0
+    @explosion.update(frame_time) unless @x == 0 && @y == 0
+    @dust.update(frame_time)
 
     if LibRay.mouse_button_pressed?(LibRay::MOUSE_LEFT_BUTTON)
       @x = LibRay.get_mouse_x
       @y = LibRay.get_mouse_y
-      @sprite.restart
+      @explosion.restart
     end
   end
 
@@ -49,7 +61,9 @@ class TexturesExample < Game
 
     @texture.draw(x: 100, y: 100)
 
-    @sprite.draw(x: @x, y: @y) unless @x == 0 && @y == 0
+    @explosion.draw(x: @x, y: @y) unless @x == 0 && @y == 0
+
+    @dust.draw(x: 500, y: 500)
   end
 end
 
--- src/game/sprite.cr
+++ src/game/sprite.cr
@@ -1,33 +1,72 @@
 require "./texture"
 
 class Sprite
-  getter name : Symbol
   getter texture : Texture
-  getter frames : Int32
+  getter frame_time : Float32
   getter cols : Int32
   getter rows : Int32
+
+  getter name : Symbol
+  getter frames : Int32
   getter width : Int32
   getter height : Int32
-  getter frame_time : Float32
   property fps : Int32
   getter? loops
+  getter start_frame : Int32
+  getter end_frame : Int32
 
   @@sprites = Hash(Symbol, Sprite).new
 
   FPS = 24
 
-  alias SpriteData = {name: Symbol, filename: String, width: Int32, height: Int32, fps: Int32, loops: Bool}
-  alias SpriteDataNoLoops = {name: Symbol, filename: String, width: Int32, height: Int32, fps: Int32}
-  alias SpriteDataNoFPS = {name: Symbol, filename: String, width: Int32, height: Int32, loops: Bool}
-  alias SpriteDataNoFPSNoLoops = {name: Symbol, filename: String, width: Int32, height: Int32}
+  alias SpriteDataFull = {name: Symbol, filename: String, width: Int32, height: Int32, fps: Int32, loops: Bool, start_frame: Int32, end_frame: Int32}
+  alias SpriteDataNoFPS = {name: Symbol, filename: String, width: Int32, height: Int32, loops: Bool, start_frame: Int32, end_frame: Int32}
+  alias SpriteDataNoLoops = {name: Symbol, filename: String, width: Int32, height: Int32, fps: Int32, start_frame: Int32, end_frame: Int32}
+  alias SpriteDataNoStartEndFrames = {name: Symbol, filename: String, width: Int32, height: Int32, fps: Int32, loops: Bool}
+  alias SpriteDataNoFPSNoStartEndFrames = {name: Symbol, filename: String, width: Int32, height: Int32, loops: Bool}
+  alias SpriteDataNoLoopsNoStartEndFrames = {name: Symbol, filename: String, width: Int32, height: Int32, fps: Int32}
+  alias SpriteDataNoFPSNoLoopsNoStartEndFrames = {name: Symbol, filename: String, width: Int32, height: Int32}
 
-  def initialize(@name, filename : String, @width, @height, @fps = FPS, @loops = true)
+  SPRITE_DATA_TYPES = [
+    SpriteDataFull,
+    SpriteDataNoFPS,
+    SpriteDataNoLoops,
+    SpriteDataNoStartEndFrames,
+    SpriteDataNoFPSNoStartEndFrames,
+    SpriteDataNoLoopsNoStartEndFrames,
+    SpriteDataNoFPSNoLoopsNoStartEndFrames,
+  ]
+
+  macro define_sprite_data_union
+    alias SpriteDataAny = {% for t in SPRITE_DATA_TYPES[0..-2] %} {{t}} | {% end %} {{SPRITE_DATA_TYPES[-1]}}
+  end
+
+  define_sprite_data_union
+
+  macro define_load_sprite_data
+    {% for t in SPRITE_DATA_TYPES %}
+      def self.load(sprite_data : {{t}})
+        load(**sprite_data)
+      end
+    {% end %}
+  end
+
+  def initialize(@name, filename : String, @width, @height, @fps = FPS, @loops = true, start_frame = nil, end_frame = nil)
     @texture = Texture.new
     @texture = Texture.get(filename)
 
     @cols = (@texture.width / @width).to_i
     @rows = (@texture.height / @height).to_i
-    @frames = @cols * @rows
+
+    if start_frame && end_frame
+      @start_frame = start_frame.to_i
+      @end_frame = end_frame.to_i
+      @frames = @end_frame - @start_frame
+    else
+      @start_frame = 0
+      @frames = @cols * @rows
+      @end_frame = @frames - 1
+    end
 
     @frame_time = 0_f32
   end
@@ -48,34 +87,31 @@ class Sprite
   end
 
   # Load multiple sprites
-  def self.load(sprites : Array(SpriteData | SpriteDataNoFPS | SpriteDataNoLoops | SpriteDataNoFPSNoLoops))
-    sprites.each { |sprite| load(sprite) }
+  def self.load(sprites : Array(SpriteDataAny))
+    sprites.each { |sprite_data| load(sprite_data) }
   end
 
-  # Load a sprite
-  def self.load(sprite : SpriteData)
-    load(name: sprite[:name], filename: sprite[:filename], width: sprite[:width], height: sprite[:height], fps: sprite[:fps], loops: sprite[:loops])
-  end
+  define_load_sprite_data
 
-  def self.load(sprite : SpriteDataNoLoops)
-    load(name: sprite[:name], filename: sprite[:filename], width: sprite[:width], height: sprite[:height], fps: sprite[:fps])
-  end
-
-  def self.load(sprite : SpriteDataNoFPS)
-    load(name: sprite[:name], filename: sprite[:filename], width: sprite[:width], height: sprite[:height], loops: sprite[:loops])
-  end
-
-  def self.load(sprite : SpriteDataNoFPSNoLoops)
-    load(name: sprite[:name], filename: sprite[:filename], width: sprite[:width], height: sprite[:height])
-  end
-
-  def self.load(name, filename, width, height, fps = FPS, loops = true)
+  def self.load(name, filename, width, height, fps = FPS, loops = true, start_frame = nil, end_frame = nil)
     if @@sprites.has_key?(name)
       puts "sprite already loaded: #{name}" if Game::DEBUG
+
       return @@sprites[name]
     else
       puts "sprite initializing: #{name}" if Game::DEBUG
-      sprite = Sprite.new(name: name, filename: filename, width: width, height: height, fps: fps, loops: loops)
+
+      sprite = Sprite.new(
+        name: name,
+        filename: filename,
+        width: width,
+        height: height,
+        fps: fps,
+        loops: loops,
+        start_frame: start_frame,
+        end_frame: end_frame
+      )
+
       @@sprites[name] = sprite
     end
 
@@ -97,7 +133,7 @@ class Sprite
   end
 
   def done?
-    frame >= frames - 1
+    frame >= end_frame
   end
 
   def restart
@@ -105,7 +141,7 @@ class Sprite
   end
 
   def frame
-    (@frame_time * @fps).to_i
+    start_frame + (@frame_time * @fps).to_i
   end
 
   def col
